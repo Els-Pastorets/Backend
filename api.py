9299cf838b5d4138c1ebc172b370624554921d70
guillempuigcomerma <56453711+guillempuigcomerma@users.noreply.github.com>
2019-10-12 02:38:01 +0200

    api tmb test

---
+++ api.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+
+#declarem la url de la api
+url = 'https://api.tmb.cat/v1/transit/linies/bus/109/parades?app_id=b6f70443&app_key=8754120bc4e20bc949df9fd773b741e6'
+data = requests.get(url)
+
+#data = json.load(json_obj)
+
+print(data.json())
