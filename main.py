b5d82af1e41e3a9be1bedee025a5961e25f5ec23
guillempuigcomerma <56453711+guillempuigcomerma@users.noreply.github.com>
2019-10-12 06:31:02 +0200

    coordenades

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/linea/{item_id}")
+@app.get("/linies/{item_id}")
 async def read_item(item_id):
     api_id = 'b6f70443';
     api_key= '8754120bc4e20bc949df9fd773b741e6';
-    url='https://api.tmb.cat/v1/transit/'+item_id+'?app_id='+api_id+'&app_key='+api_key
+    url='https://api.tmb.cat/v1/transit/linies/'+item_id+'?app_id='+api_id+'&app_key='+api_key
     data=requests.get(url)
-    return data.json()
+    return data.json()['features'][0]['geometry']
